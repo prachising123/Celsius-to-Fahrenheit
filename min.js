528021b30eaa9cf48c94b09ef2eeba8813c948cb
prachi singh <prachisingh7921@gmail.com>
2023-06-30 11:44:42 +0530

    first commit

---
+++ min.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to convert Celsius to Fahrenheit.
+
+var farh;
+var cel;
+cel=prompt("Enter value in Celsius");
+farh=cel*1.8+32;
+alert(farh);
+document.write("Temperature converted in Fahrenheit:" + F);
